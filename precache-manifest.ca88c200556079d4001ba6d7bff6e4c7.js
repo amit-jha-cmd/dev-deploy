7627dae55a3124517c47170a0a3ff69b91657620
amit <amit.jha6700@gmail.com>
2020-04-27 15:33:16 +0530

    init

---
+++ precache-manifest.ca88c200556079d4001ba6d7bff6e4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4946c4c06c5185af955b36d1a2f19c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0fb931eea845e6c31706",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b6524ff850b1e5f0065b",
+    "url": "/static/js/2.7f8a73cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f8a73cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fb931eea845e6c31706",
+    "url": "/static/js/main.04a6b00e.chunk.js"
+  },
+  {
+    "revision": "c83c0c5c57a3b01dd5db",
+    "url": "/static/js/runtime-main.6b1a6545.js"
+  }
+]);
